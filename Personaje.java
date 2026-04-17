d6e4e563b09668d3cd17dc322cb4f1991fbbebf8
AlexanderUniajc <jalexandervelez@estudiante.uniajc.edu.co>
2026-04-17 21:37:22 +0000

    Creamos la clase personaje

---
+++ Personaje.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Personaje {
+    private String nombre;
+    private int puntosDeVida;
+    private final int MAX_DANO = 30;
+    private final int MIN_DANO = 10;
+
+    // Constructor
+    public Personaje(String nombre) {
+        this.nombre = nombre;
+        this.puntosDeVida = 100;
+    }
+
+    // Metodo para atacar
+    public void atacar(Personaje oponente) {
+        Random rand = new Random();
+        int dano = rand.nextInt((MAX_DANO - MIN_DANO) + 1) + MIN_DANO;
+        oponente.recibirDano(dano);
+        System.out.println(this.nombre + " ataca a " + oponente.getNombre() + 
+                           " causando " + dano + " puntos de dano.");
+    }
+
+    // Recibir daño
+    public void recibirDano(int dano) {
+        this.puntosDeVida -= dano;
+        if (this.puntosDeVida < 0) {
+            this.puntosDeVida = 0;
+        }
+    }
+
+    public boolean estaVivo() {
+        return this.puntosDeVida > 0;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getPuntosDeVida() {
+        return this.puntosDeVida;
+    }
+}
